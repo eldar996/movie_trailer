73a94c6e00950140aa2944369b05fb24b6714d42
Eldar AKopiyanov <peacemakerkg@gmail.com>
2018-05-29 22:25:36 -0400

    PEP8 compliant

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,14 @@
 import fresh_tomatoes
 import media
 
-#Movie instances that contains movie titles, box art, poster images,
-#movie trailer URLs
+# Movie instances that contains movie titles, box art, poster images,
+# movie trailer URLs
+
+
 coco = media.Movie("Coco",
                    "The story follows a 12-year-old boy named Miguel Rivera "
                    "who is accidentally transported to the land of the dead",
-                   "https://vignette.wikia.nocookie.net/disney/images/1/14/Coco_poster.png/revision/latest?cb=20170912170727", # NOQA
+                   "https://vignette.wikia.nocookie.net/disney/images/1/14/Coco_poster.png/revision/latest?cb=20170912170727",  # NOQA
                    "https://www.youtube.com/watch?v=Rvr68u6k5sI")
 
 up = media.Movie("Up",
@@ -17,25 +19,25 @@ up = media.Movie("Up",
                  "https://www.youtube.com/watch?v=qas5lWp7_R0")
 
 wall_e = media.Movie("WALL-E",
-                   "In the distant future, a small waste-collecting robot"
-                   " inadvertently embarks on a space journey that will "
-                   "ultimately decide the fate of mankind.",
-                   "https://pixar-planet.fr/wp-content/uploads/2010/04/affiche-walle-40.jpg", # NOQA
-                   "https://www.youtube.com/watch?v=qGBZWbg_26A")
+                     "In the distant future, a small waste-collecting robot"
+                     " inadvertently embarks on a space journey that will "
+                     "ultimately decide the fate of mankind.",
+                     "https://pixar-planet.fr/wp-content/uploads/2010/04/affiche-walle-40.jpg",  # NOQA
+                     "https://www.youtube.com/watch?v=qGBZWbg_26A")
 
 big_hero6 = media.Movie("Big Hero 6",
-                   "The special bond that develops between plus-sized "
-                   "inflatable robot Baymax, and prodigy Hiro Hamada, who t"
-                   "eam up with a group of friends to form a band of "
-                   "high-tech heroes.",
-                   "https://images-na.ssl-images-amazon.com/images/I/81%2BMTze25KL._SL1500_.jpg", # NOQA
-                   "https://www.youtube.com/watch?v=z3biFxZIJOQ")
+                        "The special bond that develops between plus-sized "
+                        "inflatable robot Baymax, and prodigy Hiro Hamada, "
+                        "who team up with a group of friends to form a band "
+                        "of high-tech heroes.",
+                        "https://images-na.ssl-images-amazon.com/images/I/81%2BMTze25KL._SL1500_.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=z3biFxZIJOQ")
 
 dragon = media.Movie("How to Train Your Dragon",
                    "The story takes place in a mythical Viking world where a y"
                    "oung Viking teenager named Hiccup aspires to follow his tr"
                    "ibe's tradition of becoming a dragon slayer",
-                   "http://cdn.collider.com/wp-content/uploads/how-to-train-your-dragon-2-poster2.jpg", # NOQA
+                   "http://cdn.collider.com/wp-content/uploads/how-to-train-your-dragon-2-poster2.jpg",  # NOQA
                    "https://youtu.be/oKiYuIsPxYk?t=6s")
 
 ferdinand = media.Movie("Ferdinand",
@@ -43,7 +45,7 @@ ferdinand = media.Movie("Ferdinand",
                    "p in rural Spain after his father never returns from a sh"
                    "owdown with a matador. Adopted by a girl who lives on a fa"
                    "rm,",
-                   "https://i.pinimg.com/originals/74/54/57/745457295a72883a4d3fc516adbc03e2.jpg", # NOQA
+                   "https://i.pinimg.com/originals/74/54/57/745457295a72883a4d3fc516adbc03e2.jpg",  # NOQA
                    "https://youtu.be/YLn1_S3JJfg")
 
 brave = media.Movie("Brave",
@@ -51,13 +53,13 @@ brave = media.Movie("Brave",
                    "ch, is given a bow and arrow by her father, King Fergus, "
                    "for her 6th birthday. Her mother, Queen Elinor, disagrees"
                    " with the present",
-                   "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-7119-1mm8kcq_30f3d3a4.jpeg?region=0,0,1000,1409", # NOQA
+                   "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-7119-1mm8kcq_30f3d3a4.jpeg?region=0,0,1000,1409",  # NOQA
                    "https://youtu.be/TEHWDA_6e3M?t=7s")
 
 monsters = media.Movie("Monsters",
                    "In order to power the city, monsters have to scare child"
                    "ren so that they scream",
-                   "https://vignette.wikia.nocookie.net/disney/images/f/fe/1000px-Monsters_inc_ver1_xlg.jpeg/revision/latest?cb=20160202161958", # NOQA
+                   "https://vignette.wikia.nocookie.net/disney/images/f/fe/1000px-Monsters_inc_ver1_xlg.jpeg/revision/latest?cb=20160202161958",  # NOQA
                    "https://www.youtube.com/watch?v=QxrQ6BaijAY")
 
 inside_out = media.Movie("Inside Out",
@@ -65,11 +67,11 @@ inside_out = media.Movie("Inside Out",
                    "moved to San Francisco, her emotions - Joy, Fear, Anger,"
                    " Disgust and Sadness - conflict on how best to navigate "
                    "a new city, house, and school.",
-                   "https://vignette.wikia.nocookie.net/disney/images/f/fb/Inside-Out-60.png/revision/latest?cb=20150304211113", # NOQA
+                   "https://vignette.wikia.nocookie.net/disney/images/f/fb/Inside-Out-60.png/revision/latest?cb=20150304211113",  # NOQA
                    "https://www.youtube.com/watch?v=QxrQ6BaijAY")
 # Movie variable contains all movies
 movies = [coco, up, wall_e, big_hero6, dragon, ferdinand, brave, monsters,
-inside_out]
+          inside_out]
 
 # fresh_tomatoes generates HTML file with a Movie trailers
 # and opens up on browser
--- media.py
+++ media.py
@@ -5,8 +5,8 @@ class Movie():
 
     Creates constractor for a movie class to create instances of movie
     """
-    def __init__(self,movie_title, movie_storyline, poster_image,
-        trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
